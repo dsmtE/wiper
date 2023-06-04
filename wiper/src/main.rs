420224ee800e7613354dfcb26390ac81462c66d9
dsmtE <desmet.enguerrand@gmail.com>
2023-06-04 22:20:30 +0200

    ⚰️ Remove test code

--- wiper/src/main.rs
+++ wiper/src/main.rs
@@ -7,29 +7,10 @@ use wiper::io::handler::IoAsyncHandler;
 use wiper::io::IoEvent;
 use wiper::start_ui;
 
-use wiper::utils::walker::{
-    count_and_size, get_dir_list_from_path, is_node_modules,
-};
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Arguments::parse();
     log::debug!("{:?}", args);
-
-    // Test
-    let entries = get_dir_list_from_path(&args.root_path, &is_node_modules)
-        .collect::<Vec<_>>();
-
-    for entry in entries {
-        let path = entry.path();
-        let (count, size) = count_and_size(path);
-        println!(
-            "path : {}, files count: {} size: {:.2}MB",
-            path.display(),
-            count,
-            size as f32 / 1000000.0
-        );
-    }
 
     let (sync_io_tx, mut sync_io_rx) = tokio::sync::mpsc::channel::<IoEvent>(100);
 
